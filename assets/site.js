f6450c03774b235a355b6d417d169824618325af
chipperbecker-droid <chipperbecker@gmail.com>
2026-06-27 13:41:35 -0400

    Icon only

--- assets/site.js
+++ assets/site.js
@@ -47,11 +47,9 @@ if (!document.querySelector('.mobile-contact-actions')) {
   mobileContactActions.innerHTML = `
     <a class="mobile-contact-action" href="tel:+18436816588" aria-label="Call Gary Penrod and Associates">
       <svg aria-hidden="true" viewBox="0 0 24 24"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.8 19.8 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.8 19.8 0 0 1 2.12 4.18 2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.13.96.36 1.9.68 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.91.32 1.85.55 2.81.68A2 2 0 0 1 22 16.92Z"/></svg>
-      <span>Call</span>
     </a>
     <a class="mobile-contact-action" href="mailto:gary@garypenrod.com" aria-label="Email Gary Penrod and Associates">
       <svg aria-hidden="true" viewBox="0 0 24 24"><path d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2Z"/><path d="m22 6-10 7L2 6"/></svg>
-      <span>Email</span>
     </a>
   `;
   document.body.append(mobileContactActions);
